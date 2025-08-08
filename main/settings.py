782a5fc1b634b7f195d10e4820bb86b473e8ac3f
deju12 <dejutesfa12@gmail.com>
2025-08-09 09:44:46 +1000

    allow host *

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-&76jw-uc1d-5)d83bn6h12io0smw&_vfaf=-4w=cc0y^ge_6t6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

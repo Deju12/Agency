c4cea855b41f2076d60ddd5e98090c1cd7916bff
deju12 <dejutesfa12@gmail.com>
2025-08-09 12:16:02 +1000

    static issue fix

--- main/settings.py
+++ main/settings.py
@@ -51,6 +51,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -58,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'main.urls'
 STATICFILES_DIRS = [BASE_DIR / "app/static"]
 TEMPLATES = [

afbcc6be854b9da4143ca187a5af6d4dbec28826
deju12 <dejutesfa12@gmail.com>
2025-08-09 12:05:35 +1000

    static issue fix

--- main/settings.py
+++ main/settings.py
@@ -137,7 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'app/static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'app' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
